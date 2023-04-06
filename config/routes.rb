629a5e4e3d454af8ddc4d74525b48843a735afbc
Dev Jo <idowuayanfeoluwa@gmail.com>
2023-04-06 20:13:50 +0100

    feat: init about page, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/about", to: "about#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "main#index"
 end
