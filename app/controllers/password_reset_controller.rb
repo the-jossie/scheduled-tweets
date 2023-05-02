307368f8c1c9d38fb6a6f4c8115edd21d302ffcf
Dev Jo <idowuayanfeoluwa@gmail.com>
2023-05-02 09:23:38 +0100

    wip: password reset

---
+++ app/controllers/password_reset_controller.rb
@@ -0,0 +1,16 @@
+class PasswordResetController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user.present?
+      # Send email to the user
+      PasswordMailer.with(user: @user).reset.deliver_later
+    end
+    redirect_to root_path,
+                notice:
+                  "If an account with that email was found, we have sent a link to reset your password"
+  end
+end
